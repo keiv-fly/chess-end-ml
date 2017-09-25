edbc2759f442ef22aaf31bbe598a94cc142cbb9a
keiv.fly <kv>
2017-09-25 05:27:52 +0200

    1

--- 20170917 feature_calc KRPvKRP.py
+++ 20170917 feature_calc KRPvKRP.py
@@ -10,29 +10,44 @@ board_shape=(8,8)
 
 l_fen=list(a_fen)
 
-fen = a_fen[0]
+fen = l_fen[0]
 def pieces_features(fen):
     #fen = fen [0]
     board = chess.Board(fen)
-    ftrs=np.zeros((7,*board_shape))
+    ftrs=np.zeros((19,*board_shape))
     ftrs[0, :, :] = board.turn
     pos1 = board.king(True)
-    ftrs[0,pos1 // 8,pos1 % 8] = 1
+    ftrs[1,pos1 // 8,pos1 % 8] = 1
     pos2 = board.king(False)
-    ftrs[1, pos2 // 8, pos2 % 8] = 1
-    pos3 = list(board.pieces(chess.PAWN,True))[0]
-    ftrs[2, pos3 // 8, pos3 % 8] = 1
-    ftrs[3, :, :] = np.maximum(abs((pos3 // 8) - (pos1 // 8)), abs((pos3 % 8) - (pos1 % 8)))/8
-    ftrs[4, :, :] = np.maximum(abs((pos3 // 8) - (pos2 // 8)), abs((pos3 % 8) - (pos2 % 8)))/8
-    ftrs[5, :, :] = ((pos1 // 8) - 1) / 7
-    ftrs[6, :, :] = ((pos2 // 8) - 1) / 7
-    ftrs[7, :, :] = ((pos3 // 8) - 1) / 7
+    ftrs[2, pos2 // 8, pos2 % 8] = 1
+    pos3 = list(board.pieces(chess.ROOK,True))[0]
+    ftrs[3, pos3 // 8, pos3 % 8] = 1
+    ftrs[4, :, :] = (pos3 // 8) / 7
+    pos4 = list(board.pieces(chess.ROOK,False))[0]
+    ftrs[5, pos4 // 8, pos4 % 8] = 1
+    ftrs[6, :, :] = (pos4 // 8) / 7
+
+    pos5 = list(board.pieces(chess.PAWN,True))[0]
+    ftrs[7, pos5 // 8, pos5 % 8] = 1
+    ftrs[8, :, :] = np.maximum(abs((pos5 // 8) - (pos1 // 8)), abs((pos5 % 8) - (pos1 % 8)))/8
+    ftrs[9, :, :] = np.maximum(abs((pos5 // 8) - (pos2 // 8)), abs((pos5 % 8) - (pos2 % 8)))/8
+    ftrs[10, :, :] = ((pos1 // 8) - 1) / 7
+    ftrs[11, :, :] = ((pos2 // 8) - 1) / 7
+    ftrs[12, :, :] = ((pos5 // 8) - 1) / 7
+    ftrs[13, :, :] = (pos5 % 8) / 7
+
+    pos6 = list(board.pieces(chess.PAWN,False))[0]
+    ftrs[14, pos6 // 8, pos6 % 8] = 1
+    ftrs[15, :, :] = np.maximum(abs((pos6 // 8) - (pos1 // 8)), abs((pos6 % 8) - (pos1 % 8)))/8
+    ftrs[16, :, :] = np.maximum(abs((pos6 // 8) - (pos2 // 8)), abs((pos6 % 8) - (pos2 % 8)))/8
+    ftrs[17, :, :] = ((pos6 // 8) - 1) / 7
+    ftrs[18, :, :] = (pos6 % 8) / 7
     return ftrs
 
 
-data=np.zeros((len(l_fen),8,8,8))
+data=np.zeros((len(l_fen),19,8,8))
 for i,fen in enumerate(l_fen):
     if i % 10000 == 0:
         print(i)
     data[i]=pieces_features(fen)
-np.save("data\KPvK_w_5.npy",data)
+np.save("data\KRPvKRP_19f.npy",data)
---
+++ py1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+from keras.models import load_model
+import datetime
+
+X = np.load("data/KPvK_w_5.npy")
+X = np.swapaxes(X,1,2)
+X = np.swapaxes(X,2,3)
+df = pd.read_csv("data/KPvK_1_fen_wdl.csv")
+y = (df["wdl"]==2)*1
+m=load_model(r"data/model87KPvK_temp2.h5")
+
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
+hist2 = m.fit(X, y, batch_size=256, epochs=5)
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f"))
+y_train = m.predict(X, batch_size=256, verbose=1)
+y_train_cat=(y_train>0.5)*2
+y_train_cat=y_train_cat.flatten()
+acc = np.sum(y_train_cat==df["wdl"])/y_train_cat.shape[0]
+print("\naccuracy = ", acc)
+_ = [print(x) for x in hist2.history["loss"]]
+m.summary()
+m.save(r"data\model87KPvK_temp3.h5")
