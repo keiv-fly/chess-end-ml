6f641fe350a7e46c9b5e47e2fcb839bea0f5b3e4
keiv.fly <kv>
2017-10-15 11:02:38 +0200

    1

--- lib.py
+++ lib.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import datetime
+import chess
+import chess.syzygy
 
 def features_from_table(df):
     n_rows = len(df.index)
@@ -82,3 +84,60 @@ def features_from_table(df):
     ii = ii + 1
     print(datetime.datetime.now().strftime("%H:%M:%S.%f"))
     return data
+
+def generate_table():
+    board_start = chess.Board("K1k5/P7/8/8/8/8/8/8 w - - 0 0")
+    board_start0 = chess.Board("8/8/8/8/8/8/8/8 w - - 0 0")
+
+    l_boards = []
+    l_wdl = []
+    pieces = ["K", "R", "P", "k", "r", "p"]
+    pc_num = len(pieces)
+    pieces_types = [6, 4, 1, 6, 4, 1]
+    pieces_color = [True, True, True, False, False, False]
+    pieces_chess = [chess.Piece.from_symbol(s) for s in pieces]
+    i_min = [0, 0, 1, 0, 0, 1]
+    i_max = [8, 8, 7, 8, 8, 7]
+    j_min = [0, 0, 0, 0, 0, 0]
+    j_max = [8, 8, 8, 8, 8, 8]
+
+    l_boards = []
+    l_wdl = []
+    board = board_start0.copy()
+    i_counter = 0
+    n_iter = 25000000
+    with chess.syzygy.open_tablebases(r"syzygy") as tablebases:
+        for ii in range(n_iter):
+            if i_counter % 100000 == 0:
+                print(i_counter, " of ", n_iter, ", time: ", datetime.datetime.now().strftime("%H:%M:%S.%f"))
+                print(board.fen())
+            board = board_start0.copy()
+            i_board = []
+            i_board_i_j = []
+            turn = True
+            board.turn = turn
+            i_board.append(turn)
+            i_piece = 0
+            for i_piece in range(6):
+                i = np.random.randint(i_min[i_piece], i_max[i_piece])
+                j = np.random.randint(j_min[i_piece], j_max[i_piece])
+                board.set_piece_at(i * 8 + j, pieces_chess[i_piece])
+                i_board.append(i * 8 + j)
+                i_board_i_j.append(i)
+                i_board_i_j.append(j)
+            if len(i_board) != len(set(i_board)):
+                i_counter = i_counter + 1
+                continue
+            if board.is_valid():
+                l_boards.append(i_board)
+                l_wdl.append(tablebases.probe_wdl(board))  # *(turn*2-1))
+            i_counter = i_counter + 1
+
+    df1 = pd.DataFrame(l_boards)
+    df1.columns = ["move", "K", "R", "P", "k", "r", "p"]
+    df1["wdl"] = l_wdl
+    df2 = df1.drop_duplicates()
+    print(len(df2.index))
+    print(df2.wdl.value_counts())
+    df2 = df2.reset_index(drop=True)
+    return df2
---
+++ run_6_epochs.py
@@ -0,0 +1,85 @@
+# nohup python -u run_6_epochs.py > out.txt 2>&1 &
+# tail -f out.txt
+
+print("imports")
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import Adam, SGD
+from keras.utils import to_categorical
+from keras.models import load_model
+import datetime
+from lib import features_from_table,generate_table
+
+print("code start")
+
+# generate table with positions and results (wdl)
+start_time = datetime.datetime.now()
+df=generate_table()
+end_time = datetime.datetime.now()
+print("Position generation time: ",end_time - start_time)
+
+# generate feature boards from the table above
+start_time = datetime.datetime.now()
+X=features_from_table(df)
+end_time = datetime.datetime.now()
+print("Feature boards generation time: ",end_time - start_time)
+
+# swap axis for Keras
+X = np.swapaxes(X,1,2)
+X = np.swapaxes(X,2,3)
+
+# generate 5 arrays of results for each wdl (-2,-1,0,1,2)
+y=np.zeros((len(df.index),5))
+y[:,0] = (df["wdl"]==-2)*1
+y[:,1] = (df["wdl"]==-1)*1
+y[:,2] = (df["wdl"]==0)*1
+y[:,3] = (df["wdl"]==1)*1
+y[:,4] = (df["wdl"]==2)*1
+
+# load validation feature boards
+X2 = np.load("data/KRPvKRP_16f_10K_random_test_v2_02.npy")
+X2 = np.swapaxes(X2,1,2)
+X2 = np.swapaxes(X2,2,3)
+
+# load validation result array
+df2=pd.read_hdf("data/KRPvKRP_table_10K_random_test_v2_02.h5", 'df1')
+y2=np.zeros((len(df2.index),5))
+y2[:,0] = (df2["wdl"]==-2)*1
+y2[:,1] = (df2["wdl"]==-1)*1
+y2[:,2] = (df2["wdl"]==0)*1
+y2[:,3] = (df2["wdl"]==1)*1
+y2[:,4] = (df2["wdl"]==2)*1
+
+# load the previous model
+m=load_model(r"data/model3KRPvKRP_r_temp6.h5")
+
+# change the learning rate
+sgd = SGD(lr=0.002, momentum=0.9)
+m.compile(loss='categorical_crossentropy', optimizer=sgd)
+
+# calculate 6 epochs and calculate execution time
+start_time = datetime.datetime.now()
+print(start_time.strftime("%Y-%m-%d %H:%M:%S.%f"))
+hist = m.fit(X, y, batch_size=256, epochs=6, validation_data=(X2,y2))
+end_time = datetime.datetime.now()
+m.save(r"data/model3KRPvKRP_r_temp7.h5")
+print(end_time.strftime("%Y-%m-%d %H:%M:%S.%f"))
+print("Training time: ",end_time - start_time)
+
+# ouput training and test loss
+_ = [print(x) for x in hist.history["loss"]]
+print()
+_ = [print(x) for x in hist.history["val_loss"]]
+
+# calculate and print accuracy
+y_train = m.predict(X2, batch_size=1024*2, verbose=1)
+y_train_cat=np.argmax(y_train,1)-2
+acc = np.sum(y_train_cat==df2.wdl)/y_train_cat.shape[0]
+n_errors = y_train_cat.shape[0] - np.sum(y_train_cat==df2.wdl)
+print("\n\naccuracy\n% errors\n# errors")
+print(acc)
+print(1-acc)
+print(n_errors)
